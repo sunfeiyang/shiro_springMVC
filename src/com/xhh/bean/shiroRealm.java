0e470e0f894d32ba0f464824264ae2bee52aaf16
sunfeiyang <sfy5941@163.com>
2019-05-23 11:05:43 +0800

    shrio_springMVC init

---
+++ src/com/xhh/bean/shiroRealm.java
@@ -0,0 +1,23 @@
+package com.xhh.bean;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+public class shiroRealm implements Realm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
